5ab5386fcc835fd4813caf2324095b901c3d3439
ibrahim fikry <ibrahim.fikry2050@gmail.com>
2022-05-13 14:03:21 +0200

    end-task

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 setInterval(setclock, 1000)
-const hourHand = document.querySelector('[data-hour-hand]')
+
+
+const secondHand = document.querySelector('[data-second-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
-const secondHand = document.querySelector('[data-hour-hand]')
+const hourHand = document.querySelector('[data-hour-hand]')
 
 function setclock() {
     var currentDate = new Date();
     var secondRatio = currentDate.getSeconds() / 60
     var minuteRatio = (secondRatio + currentDate.getMinutes()) / 60
     var hourRatio = (minuteRatio + currentDate.getHours()) / 12
-    setRotation(hourHand, hourRatio)
-    setRotation(minuteHand, minuteRatio)
     setRotation(secondHand, secondRatio)
+    setRotation(minuteHand, minuteRatio)
+    setRotation(hourHand, hourRatio)
+
 }
 
 function setRotation(element, rotationRatio) {
 
-    element.style.Property('--rotation', rotationRatio * 360)
+    element.style.setProperty('--rotation', rotationRatio * 360)
 }
 setclock()
